a043716f573851580c702a7ff622c456269157bb
sshtubey <sshtubei@evolutiongaming.com>
2020-08-30 23:33:37 +0300

    Add toString for PlayerWithRank

--- src/main/java/com/sss/holdem/player/PlayerWithRank.java
+++ src/main/java/com/sss/holdem/player/PlayerWithRank.java
@@ -2,9 +2,11 @@ package com.sss.holdem.player;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class PlayerWithRank {
     private final int rank;
     private final Player player;
